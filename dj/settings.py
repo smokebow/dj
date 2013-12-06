6e72851f7eed03f6ba6bd21dd79b32c52dcb9e4d
andy <andy@u1.(none)>
2013-12-06 21:26:38 +0400

    add gitignore

--- dj/settings.py
+++ dj/settings.py
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ('assets', '/home/tanua/PycharmProjects/dj/static'),
+    ('assets', '/home/andy/PycharmProjects/dj/static'),
 )
 
 # List of finder classes that know how to find static files in
