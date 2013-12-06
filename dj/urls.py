c3b4d707e9133d06de8bfb6cdda321a7d30b63ee
«smokebow» <smokebow@gmail.com>
2013-12-06 16:18:53 +0400

    second commit

---
+++ dj/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+import views
+from django.views.generic import TemplateView
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'dj.views.home', name='home'),
+    # url(r'^dj/', include('dj.foo.urls')),
+    url(r'^$', TemplateView.as_view (template_name='index.html')),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
